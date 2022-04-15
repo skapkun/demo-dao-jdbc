853cc24697a12b68f6067fe823913450c5850c9e
Carla C <skapkun@gmail.com>
2022-04-15 17:18:11 -0400

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,8 @@
+package application;
+import modelEntities.Department;
+public class Program {
+    public static void main (String [] args){
+        Department obj = new Department (1, "Books"); 
+        System.out.println(obj);
+    }
+}
