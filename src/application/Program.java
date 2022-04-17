baf3190a16772ab86524bad6696b98544e7fb5f9
Carla C <skapkun@gmail.com>
2022-04-16 22:41:42 -0400

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,10 +6,14 @@ import modelEntities.Department;
 import modelEntities.Seller;
 public class Program {
     public static void main (String [] args){
-        Department obj = new Department (1, "Books"); 
+        /*Department obj = new Department (1, "Books"); 
         System.out.println(obj);
         Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        
+        */
+        
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
--- src/modelDao/DaoFactory.java
+++ src/modelDao/DaoFactory.java
@@ -1,10 +1,11 @@
 package modelDao;
 
+import db.DB;
 import modelDao.modelDaoImpl.SellerDadoImplJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
-        return new SellerDadoImplJDBC();
+        return new SellerDadoImplJDBC(DB.getConnection());
     }
     
 }
